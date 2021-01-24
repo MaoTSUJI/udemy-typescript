d84092977a9fd45e4df0138352a30ef623ced886
MaoTSUJI <tsuji.mao36@gmail.com>
2021-01-24 11:28:23 +0900

    get UserInfo

--- section9/src/app.ts
+++ section9/src/app.ts
@@ -1,20 +1,92 @@
+/* autbind decorator */
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundfn = originalMethod.bind(this);
+      return boundfn;
+    },
+  };
+  return adjDescriptor;
+}
+
+/* Project class */
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true);
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      "#manday"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void{
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredManday = this.mandayInputElement.value;
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredManday.trim().length === 0
+    ) {
+      alert('入力値がただしくありません。再度おためしください。');
+    } else {
+      return [enteredTitle, enteredDescription, +enteredManday];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.mandayInputElement.value = '';
+  }
+
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, manday] = userInput;
+      console.log(title, desc, manday);
+      this.clearInputs();
+    }
+  }
+
+  private configure() {
+    // this.element.addEventListener("submit", this.submitHandler);  // ここではsubmitHandlerの中のthisはeventを指してしまう.
+    // this.element.addEventListener("submit", this.submitHandler.bind(this)); // submitHandlerの内側でのthisを、このクラスのthisとして指定してやる
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
